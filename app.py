8467833c0ecfb96a68e53d1cc001bd30b015f43c
khush92212 <aaltufaltu5125@gmail.com>
2026-02-15 15:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,12 @@ st.title("Salary prediction app")
 
 
 age = st.number_input("Age",18,60)
-gender = st.selectbox("Gender", encoder["Gender"].classes_)
 education= st.selectbox("Education Level" , encoder["Education Level"].classes_)
 job_title = st.selectbox("Job Title" , encoder["Job Title"].classes_)
 years_of_exp = st.number_input("Years of Experience" ,0,40)
 
 df = pd.dataFrame({
     "Age":[age],
-    "Gender":[gender],
     "Education Level":[education],
     "Job Title":[job_title ],
     "Years of Experience":[years_of_exp]
